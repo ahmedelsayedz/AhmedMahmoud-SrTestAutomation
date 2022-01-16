66f2249a151eb07845ccb26b4f366803c2218224
Ahmed Mahmoud <45901396+ahmedelsayedz@users.noreply.github.com>
2022-01-17 00:12:02 +0200

    Update BaseTests.java

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -18,6 +18,7 @@ public class BaseTests {
     @BeforeClass
     public void setUp(){
         ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.setHeadless(true);
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver(chromeOptions);
         driver.manage().window().maximize();
